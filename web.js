60827776ab5d1f2ab25b7eced4005405ddc1ec42
Jake Gray <grayjake14@gmail.com>
2018-10-22 13:28:15 -0600

    add endpoint for ephemeral key creation

--- web.js
+++ web.js
@@ -8,14 +8,24 @@ app.listen(PORT, ()=>{
     console.log("Server up and listening on port: " + PORT)
 })
 app.get('/',(req,res)=>{
-    res.send('This is my app!')
+    res.send('This is my app! Hell yeah!')
 })
 
-app.post ('/ephemeral_key',(req,res)=>{
+app.post('/ephemeral_key',(req,res)=>{
     const stripeVersion = req.query.api_version
     if (!stripe_version) {
         res.status(400).end();
+        res.send("No Stripe version")
         return;
       }
-    console.log(stripeVersion)
+      // This function assumes that some previous middleware has determined the
+  // correct customerId for the session and saved it on the request object.
+  stripe.ephemeralKeys.create(
+    {customer: req.customerId},
+    {stripe_version: stripe_version}
+  ).then((key) => {
+    res.status(200).json(key);
+  }).catch((err) => {
+    res.status(500).end();
+  });
 })
