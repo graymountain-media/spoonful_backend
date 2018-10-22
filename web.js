e7c7f48c769cd88d7630485e245256aca2607f67
Jake Gray <grayjake14@gmail.com>
2018-10-22 13:04:04 -0600

    fix get / issue

--- web.js
+++ web.js
@@ -5,6 +5,9 @@ const stripe = require('stripe')
 app.listen(3003, ()=>{
     console.log("Server up and listening on port: 3003")
 })
+app.get('/',(req,res)=>{
+    res.send('This is my app!')
+})
 
 app.post ('/ephemeral_key',(req,res)=>{
     const stripeVersion = req.query.api_version
