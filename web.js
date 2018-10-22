4a40fd33b4e4bb180bbdfeabf919700ef775af84
Jake Gray <grayjake14@gmail.com>
2018-10-22 13:01:12 -0600

    add stripe module

--- web.js
+++ web.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const app = express()
- 
+const stripe = require('stripe')
+
+app.listen(3003, ()=>{
+    console.log("Server up and listening on port: 3003")
+})
+
+app.post ('/ephemeral_key',(req,res)=>{
+    const stripeVersion = req.query.api_version
+    if (!stripe_version) {
+        res.status(400).end();
+        return;
+      }
+    console.log(stripeVersion)
+})
