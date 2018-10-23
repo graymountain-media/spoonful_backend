513ab6d707c924dd868ef30114cdad8d41158d2f
Jake Gray <grayjake14@gmail.com>
2018-10-23 12:27:36 -0600

    fix variable for stripe version in ephemeral keys

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/ephemeral_keys',(req,res)=>{
-    const stripe_Version = req.query.api_version
+    const stripe_version = req.query.api_version
     if (!stripe_version) {
         res.status(400).end();
         res.send("No Stripe version")
