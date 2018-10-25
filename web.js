0a75c08761d60f60bbaf28a5f0821cd8c62c7226
Jake Gray <grayjake14@gmail.com>
2018-10-25 13:49:51 -0600

    use body parser

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const stripe = require('stripe')('sk_test_4d2d71YdjTMjxJJVknsPJcvO')
-
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3003
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended : true
+}))
+
 
 app.listen(PORT, ()=>{
     console.log("Server up and listening on port: " + PORT)
@@ -20,7 +26,7 @@ app.post('/ephemeral_keys',(req,res)=>{
       }
       // This function assumes that some previous middleware has determined the
       // correct customerId for the session and saved it on the request object.
-      stripe.ephemeral.create(
+      stripe.ephemeralKeys.create(
         {customer: req.customerId},
         {stripe_version: stripe_version}
       ).then((key) => {
