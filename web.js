ac92dbde6d19438ce47f0e3fd830b35b51046fd5
Jake Gray <grayjake14@gmail.com>
2018-12-15 01:57:22 -0700

    Remove stripe and implement Braintree clientToken

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
-const stripe = require('stripe')('sk_test_4d2d71YdjTMjxJJVknsPJcvO')
+//const stripe = require('stripe')('sk_test_4d2d71YdjTMjxJJVknsPJcvO')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3003
+
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "k2z7jdk8znqf6y3p",
+  publicKey: "kg47c9snxby9prwm",
+  privateKey: "9530676083d31b2f88e43964cb59d18a"
+});
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -13,10 +22,23 @@ app.use(bodyParser.urlencoded({
 app.listen(PORT, ()=>{
     console.log("Server up and listening on port: " + PORT)
 })
+
+gateway.clientToken.generate({
+  customerId: aCustomerId
+}, function (err, response) {
+  var clientToken = response.clientToken
+});
+
 app.get('/',(req,res)=>{
     res.send('This is my app! Hell yeah!')
 })
 
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+/*
 app.post('/ephemeral_keys',(req,res)=>{
     const stripe_version = req.query.api_version
     console.log('Stripe Version: ' + stripe_version)
@@ -57,3 +79,4 @@ app.post('/charge',(req,res)=>{
     }
   })
 })
+*/
